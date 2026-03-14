d91acc8ac086ffff3ba0abbde4792956a5396d0e
maurocosentino <mauroxcosentino@gmail.com>
2026-03-14 14:57:28 -0300

    feat: add HabitRepository interface

--- app/src/main/java/com/mauro/habittracker/core/domain/repository/HabitRepository.kt
+++ app/src/main/java/com/mauro/habittracker/core/domain/repository/HabitRepository.kt
@@ -1,2 +1,13 @@
 package com.mauro.habittracker.core.domain.repository
 
+import com.mauro.habittracker.core.domain.model.Habit
+import com.mauro.habittracker.core.domain.model.HabitLog
+import kotlinx.coroutines.flow.Flow
+
+interface HabitRepository{
+    fun getHabits() : Flow<List<Habit>>
+    suspend fun insertHabit(habit: Habit)
+    suspend fun deleteHabit(habit: Habit)
+    fun getLogsForHabit(habitId: Long) : Flow<List<HabitLog>>
+    suspend fun insertLog(log: HabitLog)
+}
