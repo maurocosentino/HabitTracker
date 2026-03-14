11bc2090443166fbdb048533455d3990cf2059ba
maurocosentino <mauroxcosentino@gmail.com>
2026-03-14 19:52:43 -0300

    feat: add AppDatabase with Room setup

--- app/src/main/java/com/mauro/habittracker/data/local/AppDatabase.kt
+++ app/src/main/java/com/mauro/habittracker/data/local/AppDatabase.kt
@@ -1,2 +1,15 @@
 package com.mauro.habittracker.data.local
 
+import androidx.room.RoomDatabase
+import androidx.room3.Database
+import com.mauro.habittracker.data.local.dao.HabitDao
+import com.mauro.habittracker.data.local.dao.HabitLogDao
+import com.mauro.habittracker.data.local.entity.HabitEntity
+import com.mauro.habittracker.data.local.entity.HabitLogEntity
+
+@Database(entities = [HabitEntity::class, HabitLogEntity::class], version = 1)
+abstract class AppDatabase : RoomDatabase() {
+    abstract fun habitDao(): HabitDao
+    abstract fun habitLogDao(): HabitLogDao
+
+}
