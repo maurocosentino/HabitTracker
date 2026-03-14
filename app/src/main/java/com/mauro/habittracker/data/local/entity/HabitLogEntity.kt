3d9b67619f0fe7b3d61713a8d1d1912f575d1ef2
maurocosentino <mauroxcosentino@gmail.com>
2026-03-14 17:42:28 -0300

    feat: add Room entities HabitEntity and HabitLogEntity

--- app/src/main/java/com/mauro/habittracker/data/local/entity/HabitLogEntity.kt
+++ app/src/main/java/com/mauro/habittracker/data/local/entity/HabitLogEntity.kt
@@ -3,9 +3,11 @@ package com.mauro.habittracker.data.local.entity
 import androidx.room3.Entity
 import androidx.room3.ForeignKey
 import androidx.room3.ForeignKey.Companion.CASCADE
+import androidx.room3.Index
 import androidx.room3.PrimaryKey
 
 @Entity(
+    indices = [Index(value = ["habitId", "completedDate"], unique = true)],
     foreignKeys = [
         ForeignKey(
             entity = HabitEntity::class,
@@ -16,7 +18,7 @@ import androidx.room3.PrimaryKey
     ]
 )
 data class HabitLogEntity(
-    @PrimaryKey val id: Long = 0,
+    @PrimaryKey(autoGenerate = true) val id: Long = 0,
     val habitId: Long,
     val completedDate : Long
 )
